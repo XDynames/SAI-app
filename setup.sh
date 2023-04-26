3c49644567a7ee870ed1e6e942bc8efbbfb514a7
James Bockman <39457784+XDynames@users.noreply.github.com>
2023-04-26 10:07:42 +0930

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ else
 pip3 install --find-links https://download.pytorch.org/whl/torch_stable.html \
     torch==1.11.0+cpu \
     torchvision==0.12.0+cpu \
-    torchaudio==0.8.1
+    torchaudio==0.11.0+cpu
 fi
 pip3 install git+https://github.com/facebookresearch/detectron2.git
 pip3 install -r requirements.txt
