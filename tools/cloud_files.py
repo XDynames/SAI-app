92a21c9d283fed6aed253188e3590de5c4b16990
James Bockman <james@aiml.team>
2024-01-31 14:58:47 +1030

    Image and prediction hosting

--- tools/cloud_files.py
+++ tools/cloud_files.py
@@ -2,182 +2,182 @@ HOST_URL_ROOT = "https://adelaide.figshare.com/ndownloader/files"
 
 IMAGE_DICTS = {
     "Barley": {
-        "10Dec 19": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/GKYrltfzbIFRAd0",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/k1IIwo9XsF7pxyb",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/DLZFhDjV0KFLWn6",
-        },
-        "10Dec 24": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/xsIqHKknjKLONL9",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/gbsmf5UdwqHZpXn",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/RreGojhTnCHXRW8",
-        },
-        "10Dec 29": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/TdNPt65R0b94N5X",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/CqQhXaU1SWeiSP4",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/A4u39ifUR8jnwoK",
-        },
-        "10Dec 30": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/FxotDaDeeYimlaM",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/8Ff2WTK15PJF80Q",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/d2475BLnJ1wYq9s",
-        },
-        "18Mar 28": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/A68njFxi3wdpnOC",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/0ijPkHHBtbCCqHw",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/JcL47cfBJQrQ79Q",
-        },
-        "18Mar 31": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/VhBTnlA923NFdlS",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/qdbzvQoHOn07NuM",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/k6Mz1Vm4i8XolNa",
-        },
-        "18Mar 34": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/yQjGO8oCb9q8LPY",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/yC5T76FGfOC48wQ",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/P7uxtX9WrhkjzF4",
-        },
-        "18Mar 37": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/BDYwzQXbzTKqx9F",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/YoSGgHveWnTPLel",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/An6q34m85ZiVoGW",
-        },
-        "25Feb 27": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/DNHXGmsxB9UeWG3",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/mwPUv0oanRq67DV",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/0Wnxuc0W2ryyBcP",
-        },
-        "25Feb 29": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/sZCOqbu8772OOgy",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/hAMfYLPHhCSNkYB",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/ue3qjMJsGHb76YM",
-        },
-        "25Feb 32": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/fwygto1Upd33aLk",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/AveEgXsDEuc1bpk",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/JJTAe2xI75PPvrl",
-        },
-        "25Feb 37": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/P4w4oNaN210SX41",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/XpFeN4dxuQU8eIK",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/jhvHuYmzVSP3kLA",
-        },
-        "5Mar 10": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/pF7lI2fxBd7FlJb",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/hQ535U2TmyN2iNN",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/yTbPzD0EkvbE4FR",
-        },
         "5Mar 3": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/ae3q8P2VaZOUxEq",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/mHhKDwcDhZcqQqZ",
+            "image": f"{HOST_URL_ROOT}/44323475",
+            "predictions": f"{HOST_URL_ROOT}/44323952",
             "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/i3PcfBKWdbZaKTa",
         },
         "5Mar 6": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/InaQTbbjD9rOd8l",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/RJy0vcoUseumSwv",
+            "image": f"{HOST_URL_ROOT}/44323478",
+            "predictions": f"{HOST_URL_ROOT}/44323946",
             "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/qWMAFgcwWVpHwzD",
+        },
+        "5Mar 10": {
+            "image": f"{HOST_URL_ROOT}/44323481",
+            "predictions": f"{HOST_URL_ROOT}/44323949",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/yTbPzD0EkvbE4FR",
+        },
+        "10Dec 19": {
+            "image": f"{HOST_URL_ROOT}/44323484",
+            "predictions": f"{HOST_URL_ROOT}/44323943",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/DLZFhDjV0KFLWn6",
+        },
+        "10Dec 24": {
+            "image": f"{HOST_URL_ROOT}/44323487",
+            "predictions": f"{HOST_URL_ROOT}/44323955",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/RreGojhTnCHXRW8",
+        },
+        "10Dec 29": {
+            "image": f"{HOST_URL_ROOT}/44323490",
+            "predictions": f"{HOST_URL_ROOT}/44323958",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/A4u39ifUR8jnwoK",
+        },
+        "10Dec 30": {
+            "image": f"{HOST_URL_ROOT}/44323493",
+            "predictions": f"{HOST_URL_ROOT}/44323961",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/d2475BLnJ1wYq9s",
+        },
+        "18Mar 28": {
+            "image": f"{HOST_URL_ROOT}/44323496",
+            "predictions": f"{HOST_URL_ROOT}/44323964",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/JcL47cfBJQrQ79Q",
+        },
+        "18Mar 31": {
+            "image": f"{HOST_URL_ROOT}/44323499",
+            "predictions": f"{HOST_URL_ROOT}/44323967",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/k6Mz1Vm4i8XolNa",
+        },
+        "18Mar 34": {
+            "image": f"{HOST_URL_ROOT}/44323502",
+            "predictions": f"{HOST_URL_ROOT}/44323970",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/P7uxtX9WrhkjzF4",
+        },
+        "18Mar 37": {
+            "image": f"{HOST_URL_ROOT}/44323505",
+            "predictions": f"{HOST_URL_ROOT}/44323973",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/An6q34m85ZiVoGW",
+        },
+        "25Feb 27": {
+            "image": f"{HOST_URL_ROOT}/44323508",
+            "predictions": f"{HOST_URL_ROOT}/44323976",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/0Wnxuc0W2ryyBcP",
+        },
+        "25Feb 29": {
+            "image": f"{HOST_URL_ROOT}/44323511",
+            "predictions": f"{HOST_URL_ROOT}/44323979",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/ue3qjMJsGHb76YM",
+        },
+        "25Feb 32": {
+            "image": f"{HOST_URL_ROOT}/44323514",
+            "predictions": f"{HOST_URL_ROOT}/44323982",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/JJTAe2xI75PPvrl",
+        },
+        "25Feb 37": {
+            "image": f"{HOST_URL_ROOT}/44323517",
+            "predictions": f"{HOST_URL_ROOT}/44323985",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/jhvHuYmzVSP3kLA",
         },
     },
     "Arabidopsis": {
         "1 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/AMYCHSdmOgUFbKr",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/MdorizaS3sheqTy",
+            "image": f"{HOST_URL_ROOT}/44323415",
+            "predictions": f"{HOST_URL_ROOT}/44323715",
             "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/TWcFsGslq26P4kl",
         },
-        "3 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/RJt9ny7k2PTNrgc",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/ZNYbpgk6dO0Z0QV",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/vfO9fWXoA4Q8Fkl",
-        },
-        "5 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/nTO1fFwwLDCeIUG",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/mJ0PW8wZVlh1jGN",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/UZm7440qg53LP6D",
-        },
-        "17 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/2ZC9oxmyGr6s483",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/Gst9ynP5Br1qiug",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/ZlHuAStzWyU5qYo",
-        },
-        "19 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/MShWGRrOMIWyL03",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/mqN1mMOw3pr3cCK",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/0YnGOClvjO9YyTi",
-        },
-        "6 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/cJKWUvteqEVa6gp",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/wdtt3uZoQZTKQxF",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/oL5BS5ak4YAN51d",
-        },
-        "8 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/xu9pFhsIqmA9IDW",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/bbndfcSuse9mGsn",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/rtWxr4xKlSgVX5b",
-        },
-        "10 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/6kt5YZ9b3fpWA3u",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/rqNxnkRZso2yExa",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/0b6AYTWu7xhJu4p",
-        },
-        "12 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/iSvwR8DyvYDgX7k",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/G5ufDOzkoa5MxYM",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/yLX8rbfbxNqEgre",
-        },
-        "14 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/T6vnVT726i1K0oq",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/CcQ4J1V2b2WGdbS",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/zzyjhkw6T2LjiSf",
-        },
         "2 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/rBccrjuu6k4iOP7",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/zqgGTrMSOzh6XwD",
+            "image": f"{HOST_URL_ROOT}/44323418",
+            "predictions": f"{HOST_URL_ROOT}/44323748",
             "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/U8Ftb1a2UIpMg9B",
         },
+        "3 - At": {
+            "image": f"{HOST_URL_ROOT}/44323421",
+            "predictions": f"{HOST_URL_ROOT}/44323751",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/vfO9fWXoA4Q8Fkl",
+        },
         "4 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/zYnsdy3g3jsUxwW",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/d3vMFSpZ70bXRJj",
+            "image": f"{HOST_URL_ROOT}/44323424",
+            "predictions": f"{HOST_URL_ROOT}/44323754",
             "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/nh56qzdzYHvSyxh",
         },
-        "16 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/7fb54HQb4UTkkuk",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/ndsi7XVBwCmyCye",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/HkmX2Ymi0Af0tD0",
+        "5 - At": {
+            "image": f"{HOST_URL_ROOT}/44323427",
+            "predictions": f"{HOST_URL_ROOT}/44323757",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/UZm7440qg53LP6D",
         },
-        "18 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/mPGpY9PulN5Ji2N",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/CbWmP6algznlXHg",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/K3ilt9PQAi4Jpnv",
-        },
-        "20 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/CBkNoIhqDhiGW1p",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/oYbGTFN4mktZHSK",
-            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/8XbGnv8TyDuzToN",
+        "6 - At": {
+            "image": f"{HOST_URL_ROOT}/44323430",
+            "predictions": f"{HOST_URL_ROOT}/44323760",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/oL5BS5ak4YAN51d",
         },
         "7 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/zi1YZp7vWskX7fB",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/FJDNTczv69YrTRp",
+            "image": f"{HOST_URL_ROOT}/44323433",
+            "predictions": f"{HOST_URL_ROOT}/44323763",
             "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/zPgxgWzALBxU5hY",
         },
+        "8 - At": {
+            "image": f"{HOST_URL_ROOT}/44323436",
+            "predictions": f"{HOST_URL_ROOT}/44323766",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/rtWxr4xKlSgVX5b",
+        },
         "9 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/0lnJTwBBnoe3rIJ",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/S2LZ9TraHLHFUzP",
+            "image": f"{HOST_URL_ROOT}/44323439",
+            "predictions": f"{HOST_URL_ROOT}/44323769",
             "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/1XdCO66rWhgT8m6",
         },
+        "10 - At": {
+            "image": f"{HOST_URL_ROOT}/44323442",
+            "predictions": f"{HOST_URL_ROOT}/44323772",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/0b6AYTWu7xhJu4p",
+        },
         "11 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/NAsFbPPuWQmCw8w",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/2Ip3hzmsAScLJPs",
+            "image": f"{HOST_URL_ROOT}/44323445",
+            "predictions": f"{HOST_URL_ROOT}/44323775",
             "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/aJ2XfdbjGLvH887",
         },
+        "12 - At": {
+            "image": f"{HOST_URL_ROOT}/44323448",
+            "predictions": f"{HOST_URL_ROOT}/44323778",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/yLX8rbfbxNqEgre",
+        },
         "13 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/2b6ldmBm5YKZ2Aq",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/QqqOZZPUokbrVQI",
+            "image": f"{HOST_URL_ROOT}/44323451",
+            "predictions": f"{HOST_URL_ROOT}/44323781",
             "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/30hN7bVS0XBsNW0",
         },
+        "14 - At": {
+            "image": f"{HOST_URL_ROOT}/44323454",
+            "predictions": f"{HOST_URL_ROOT}/44323784",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/zzyjhkw6T2LjiSf",
+        },
         "15 - At": {
-            "image": "https://cloudstor.aarnet.edu.au/plus/s/bjKK9bg9N5gHV5J",
-            "predictions": "https://cloudstor.aarnet.edu.au/plus/s/6S2FP38DHarRhAA",
+            "image": f"{HOST_URL_ROOT}/44323472",
+            "predictions": f"{HOST_URL_ROOT}/44323787",
             "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/2y5nDoK5j2VggKA",
+        },
+        "16 - At": {
+            "image": f"{HOST_URL_ROOT}/44323466",
+            "predictions": f"{HOST_URL_ROOT}/44323790",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/HkmX2Ymi0Af0tD0",
+        },
+        "17 - At": {
+            "image": f"{HOST_URL_ROOT}/44323460",
+            "predictions": f"{HOST_URL_ROOT}/44323793",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/ZlHuAStzWyU5qYo",
+        },
+        "18 - At": {
+            "image": f"{HOST_URL_ROOT}/44323463",
+            "predictions": f"{HOST_URL_ROOT}/44323796",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/K3ilt9PQAi4Jpnv",
+        },
+        "19 - At": {
+            "image": f"{HOST_URL_ROOT}/44323457",
+            "predictions": f"{HOST_URL_ROOT}/44323799",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/0YnGOClvjO9YyTi",
+        },
+        "20 - At": {
+            "image": f"{HOST_URL_ROOT}/44323469",
+            "predictions": f"{HOST_URL_ROOT}/44323802",
+            "ground_truth": "https://cloudstor.aarnet.edu.au/plus/s/8XbGnv8TyDuzToN",
         },
     },
 }
